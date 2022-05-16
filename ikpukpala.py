6231797a6e279c1e535d471244bf016a55137d43
Josiah J. Umezurike <j_umezurike@yahoo.com>
2022-05-16 12:13:37 -0400

    fundamentals
    Here I likened a stringed nodes to stringed coins. The flipping of coins is rife for our purpose. We need to simulate the flipping of four coins in each string. 
    The will be done for 4 different strings. We are looking at 256 outcomes. How they will be used will be explained.

---
+++ ikpukpala.py
@@ -0,0 +1,43 @@
+# Principle of 16 project. This is a harbinger of a powerful autonomy for users bringing in web3.
+import random
+print("\nWelcome to Afa Oracle casting program:");
+
+ikpukpala = ["ogbi", "akwu", "ogheli", "odii", "ululu", "aghali", "obala", "okala", 
+             "ijite", "ora", "aka", "atulukpa", "otule", "ete", "ose", "ovu"];
+print ("\nShow the ikpukpala list:\n")
+print(ikpukpala);
+
+
+numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+print ("\nShow the assigned numbers to the list:\n")
+print(numbers);
+
+print ("\nShow the truth table for 4 stringed nodes flips:\n")
+H = int;
+T = int;
+H == 0; 
+T == 1; 
+
+combination = [ 
+                ('H', 'H', 'H', 'H'), 
+                ('H', 'H', 'H', 'T'),
+                ('H', 'H', 'T', 'T'),
+                ('H', 'H', 'T', 'T'),
+                ('H', 'T', 'H', 'H'),
+                ('H', 'T', 'H', 'T'),
+                ('H', 'T', 'T', 'H'),
+                ('H', 'T', 'T', 'T'),
+                ('T', 'H', 'H', 'H'),
+                ('T', 'H', 'H', 'T'),
+                ('T', 'H', 'T', 'H'),
+                ('T', 'H', 'T', 'T'),
+                ('T', 'T', 'H', 'H'),
+                ('T', 'T', 'H', 'T'),
+                ('T', 'T', 'T', 'H'),
+                ('T', 'T', 'T', 'T')
+              ];
+print(combination);
+
+print("\n");
+
+print(combination[8]);
