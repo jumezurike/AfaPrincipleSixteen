d141e16927b9e77fba6f4472a5ea11566797a6cc
Josiah J. Umezurike <j_umezurike@yahoo.com>
2022-05-17 10:03:17 -0400

    update flipping
    node or coin flipping added

--- ikpukpala.py
+++ ikpukpala.py
@@ -1,8 +1,11 @@
 # Principle of 16 project. This is a harbinger of a powerful autonomy for users bringing in web3.
 import random
+from random import randint
+from time import sleep
+
 print("\nWelcome to Afa Oracle casting program:");
 
-ikpukpala = ["ogbi", "akwu", "ogheli", "odii", "ululu", "aghali", "obala", "okala", 
+ikpukpala = ["ogbi", "akwu", "ogheli", "odii", "ululu", "aghali", "obala", "okala",
              "ijite", "ora", "aka", "atulukpa", "otule", "ete", "ose", "ovu"];
 print ("\nShow the ikpukpala list:\n")
 print(ikpukpala);
@@ -15,11 +18,11 @@ print(numbers);
 print ("\nShow the truth table for 4 stringed nodes flips:\n")
 H = int;
 T = int;
-H == 0; 
-T == 1; 
+H == 0;
+T == 1;
 
-combination = [ 
-                ('H', 'H', 'H', 'H'), 
+combination = [
+                ('H', 'H', 'H', 'H'),
                 ('H', 'H', 'H', 'T'),
                 ('H', 'H', 'T', 'T'),
                 ('H', 'H', 'T', 'T'),
@@ -41,3 +44,22 @@ print(combination);
 print("\n");
 
 print(combination[8]);
+
+h_counts = 0;
+t_counts = 0;
+#def h_t(num_of_flips):
+num_of_flips = int(input("\nPlease enter the number of flips:"))
+
+for i in range(num_of_flips):
+    rand = randint(0,1)
+    sleep(1)
+    if rand == 0:
+            h_counts +=1;
+            print("H");
+    else:
+            print("T")
+            t_counts +=1;
+
+print("\nThe result are:")
+print("T", t_counts)
+print("H", h_counts)
